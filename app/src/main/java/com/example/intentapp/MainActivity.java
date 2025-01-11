af3b8929aec5b77d2156aef31bc8a2d1901fe80c
amit <amit1schneider@gmail.com>
2025-01-11 13:54:51 +0200

    intent 2

--- app/src/main/java/com/example/intentapp/MainActivity.java
+++ app/src/main/java/com/example/intentapp/MainActivity.java
@@ -4,12 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.intentapp.R;
-import com.example.intentapp.ShareActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
